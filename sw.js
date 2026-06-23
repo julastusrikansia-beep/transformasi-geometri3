a3913876c9bddfe57041976332123b3a7182ae54
julastusrikansia-beep <muhamadjulastusrikansiamahmud@gmail.com>
2026-06-24 00:48:33 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,28 +1,27 @@
-const CACHE_NAME = 'geometri-v2';
+const CACHE_NAME = 'geometri-v3';
 
-// Menyesuaikan dengan daftar file asli Anda di screenshot
 const assetsToCache = [
-  './',
-  './index.html',
-  './01-1-materi-pengantar.html',
-  './01-1-1-translasi.html',
-  './01-1-2-refleksi.html',
-  './01-1-3-rotasi.html',
-  './01-1-4-dilatasi.html',
-  './02-simulasi.html',
-  './03-kuis.html',
-  './assets/css/style.css',
-  './assets/css/simulasi.css',
-  './assets/css/materi-pengantar.css',
-  './assets/js/background.js',
-  './assets/js/main-menu.js',
-  './assets/js/landscape-guard.js',
-  './assets/js/kuis-data.js',
-  './assets/js/kuis-konten.js',
-  './assets/js/materi.js',
-  './assets/js/scroll-animation.js',
-  './assets/js/simulasi.js',
-  './manifest.json'
+  '/',                     
+  '/index.html',           
+  '/01-1-materi-pengantar.html',
+  '/01-1-1-translasi.html',
+  '/01-1-2-refleksi.html',
+  '/01-1-3-rotasi.html',
+  '/01-1-4-dilatasi.html',
+  '/02-simulasi.html',
+  '/03-kuis.html',
+  '/assets/css/style.css',
+  '/assets/css/simulasi.css',
+  '/assets/css/materi-pengantar.css',
+  '/assets/js/background.js',
+  '/assets/js/main-menu.js',
+  '/assets/js/landscape-guard.js',
+  '/assets/js/kuis-data.js',
+  '/assets/js/kuis-konten.js',
+  '/assets/js/materi.js',
+  '/assets/js/scroll-animation.js',
+  '/assets/js/simulasi.js',
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
